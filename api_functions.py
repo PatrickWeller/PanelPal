76ab3629e69b3effa78e9eec4af285f81b0b6ec1
PatrickWeller <patrick.weller@wales.nhs.uk>
2024-11-05 11:20:52 +0000

    Added some docstrings and made panel and build dynamic

--- api_functions.py
+++ api_functions.py
@@ -2,11 +2,17 @@ import requests
 
 def main():
     panel_id = "R293"
+    build = "GRch37"
+    
     response = get_response(panel_id)
-    print(create_locus_dictionary(response))
+    print(create_locus_dictionary(response, build))
     
 
 def get_response(panel_id):
+    """
+    Input a panel id, e.g. R293 or 293.
+    Submits an API request, and returns a JSON
+    """
     url = f"https://panelapp.genomicsengland.co.uk/api/v1/panels/{panel_id}"
     response = requests.get(url)
     if response.status_code == 200:
@@ -14,11 +20,15 @@ def get_response(panel_id):
     else:
         return f"Error: {response.status_code}"
 
-def create_locus_dictionary(api_response):
+def create_locus_dictionary(api_response, build):
+    """
+    Input a valid API response JSON and a chr build (e.g. GRch37)
+    Outputs a list of chromosomal locatins in that panel
+    """
     genes = api_response["genes"]
     location_list = []
     for gene in genes:
-        location_list.append(gene["gene_data"]["ensembl_genes"]["GRch37"]["82"]["location"])
+        location_list.append(gene["gene_data"]["ensembl_genes"][f"{build}"]["82"]["location"])
     return location_list
 
 
