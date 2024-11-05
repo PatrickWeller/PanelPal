3e12f6ceb629b336d91fe995a6be6d9893fb8cf5
RVel37 <rvelissaris@gmail.com>
2024-11-05 13:42:33 +0000

    separated functions, added print statement

--- api_query.py
+++ api_query.py
@@ -4,7 +4,8 @@ import requests
 def is_integer(id): 
     return isinstance(id, int)
 
-def get_name_version_genes(id):
+### function to get panel name and version
+def get_name_version(id):
 
     if not is_integer(id): 
         raise ValueError("The panel ID must be an integer.")
@@ -20,31 +21,36 @@ def get_name_version_genes(id):
         data = response.json()
         panel_name = data.get("name","N/A")
         panel_version = data.get("version","N/A")
-        genes = [gene["gene_data"]["gene_symbol"] for gene in data.get("genes",[])]
 
         return {
             "name": panel_name,
-            "version": panel_version,
-            "genes": genes
+            "version": panel_version
         }
 
     except requests.exeptions.RequestException as e:
         print(f"An error occurred: {e}")
 
+### function to get genes list
+def get_genes(id):
+
+    url = f"https://panelapp.genomicsengland.co.uk/api/v1/panels/{id}/"
+
+    response = requests.get(url)
+    data = response.json()
+    genes = [gene["gene_data"]["gene_symbol"] for gene in data.get("genes",[])]
+
+    return {
+        "genes": genes
+    }
+
+### Main execution block
 if __name__ == "__main__":
+    panel_id = 1
+    
+    # Get panel name and version
+    panel_info = get_name_version(panel_id)
+    print("Panel Information:", panel_info)
 
-    ### EXAMPLE PANEL ID - CAN BE SET DYNAMICALLY
-    id = 1 
-
-    results = {}
-
-    # Fetch panel information
-    panel_read_info = get_name_version_genes(id)
-    if "error" not in panel_read_info:
-        results["panel_id"] = id
-        results["name"] = panel_read_info["name"]
-        results["version"] = panel_read_info["version"]
-        results["genes"] = panel_read_info["genes"]
-    else:
-        results["error"] = panel_read_info["error"]
-
+    # Get genes
+    genes_info = get_genes(panel_id)
+    print("Genes Information:", genes_info)
