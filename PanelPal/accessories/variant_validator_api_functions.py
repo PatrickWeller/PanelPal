f9e8b5d906b09719358e4ffc0d457bcd1bf10a49
ASendellPrice <ashley.sendell.price@gmail.com>
2024-11-11 18:57:17 +0000

    improved error handling and logging in check_panel.py

---
+++ PanelPal/accessories/variant_validator_api_functions.py
@@ -0,0 +1,205 @@
+import requests
+import time
+import logging
+import subprocess
+
+def get_gene_transcript_data(gene_name, genome_build="GRCh38", max_retries=4):
+    """
+    Fetches the gene transcript data for a given gene from the Variant Validator API.
+
+    Args:
+        gene_name (str): The name of the gene to fetch transcript data for.
+        genome_build (str, optional): The genome build to use (default is "GRCh38").
+        max_retries (int, optional): The maximum number of retries in case rate limit is exceeded (error 429). 
+    Returns:
+        dict: The JSON response containing gene transcript information.
+
+    Raises:
+        Exception: If the request to the API fails (status code not 200).
+    """
+    # Base URL for the Variant Validator API endpoint
+    base_url = "https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts_v2/"
+    
+    # Construct the URL with the given gene name and genome build
+    url = f"{base_url}/{gene_name}/mane_select/refseq/{genome_build}?content-type=application%2Fjson"
+
+    retries = 0
+    
+    while retries < max_retries:
+
+        logging.info(f"Fetching data for {gene_name} (Attempt {retries+1})")
+
+        # Send the GET request to the Variant Validator API
+        response = requests.get(url)
+
+        if response.status_code == 200: # success
+            logging.info(f"Data for {gene_name} fetched successfully.")
+            return response.json() # return JSON content from API response
+        
+        # run a number of retries if request times out
+        elif response.status_code == 429:
+            wait_time = 2 ** retries # the time delay between requests exponentially increases for each new retry
+            logging.warning(f"Rate limit exceeded for {gene_name}. Retrying in {wait_time} seconds.")
+            time.sleep(wait_time) 
+            retries += 1
+        else:
+            logging.error(f"Failed to fetch gene data for {gene_name}: {response.status_code}")
+            raise Exception(f"{response.status_code}. Terminating.")
+    
+    # exception for when every attempt to fetch gene data fails
+    logging.error(f"Max retries reached for {gene_name}. The number of tries may need increasing.")
+    raise Exception(f"Max retries reached for {gene_name}. Terminating.")
+
+
+
+def extract_exon_info(gene_transcript_data):
+    """
+    Extracts exon information from the gene transcript data.
+
+    Args:
+        gene_transcript_data (dict): The JSON response containing the gene transcript data.
+
+    Returns:
+        list: A list containing exon data.
+    """
+    exon_data = []  # Initialize an empty list to store exon data as dictionaries
+
+    # Loop through each gene in the transcript data
+    for gene_data in gene_transcript_data:        
+        # Loop through each transcript associated with the gene
+        for transcript in gene_data['transcripts']:
+            # Extract chromosome information from the transcript
+            chromosome = transcript['annotations'].get('chromosome', 'Unknown')
+            
+            # Extract the reference transcript information
+            transcript_reference = transcript.get('reference', 'Unknown')
+            
+            # Extract the gene symbol
+            gene_symbol = gene_data.get('current_symbol', 'Unknown')
+            
+            # Loop through the genomic spans in the transcript (exon structures)
+            for genomic_span_key, genomic_span in transcript['genomic_spans'].items():
+                if 'exon_structure' in genomic_span:  # Ensure 'exon_structure' exists before accessing it
+                    for exon in genomic_span['exon_structure']:
+                        # Extract exon-specific information such as exon number, start, and end positions
+                        exon_number = exon.get('exon_number', None)
+                        exon_start = exon.get('genomic_start', None)
+                        exon_end = exon.get('genomic_end', None)
+                        
+                        # Store the extracted exon data in a dictionary
+                        exon_info = {
+                            "chromosome": chromosome,
+                            "exon_start": exon_start,
+                            "exon_end": exon_end,
+                            "exon_number": exon_number,
+                            "reference": transcript_reference,
+                            "gene_symbol": gene_symbol
+                        }
+                        
+                        # Append the exon data to the list of exon_data
+                        exon_data.append(exon_info)
+    
+    logging.info(f"Extracted {len(exon_data)} exons for the gene.")
+    
+    # Return the complete list of exon data
+    return exon_data
+
+
+def generate_bed_file(gene_list, panel_name, panel_version, genome_build="GRCh38"):
+    """
+    Generates a BED file containing exon data for a list of genes.
+    Uses GRCh38 by default.
+
+    Args:
+        gene_list (list): A list of gene names for which exon data is to be extracted.
+        panel_name (str): The name of the panel, used to name the output BED file.
+        genome_build (str): The genome build, used to name the output BED file.
+
+    Returns:
+        None: This directly writes to a BED file.
+    """
+    # Define the name of the output BED file based on the panel name and genome build
+    output_file = f"{panel_name}_v{panel_version}_{genome_build}.bed"
+    logging.info(f"Creating BED file: {output_file}")
+
+    # Open the BED file for writing (or create it if it doesn't exist)
+    with open(output_file, 'w') as bed_file:
+        # Iterate over the list of genes to fetch and process their transcript data
+        for gene in gene_list:
+
+            try:
+                # Fetch the transcript data for the current gene using the API
+                gene_transcript_data = get_gene_transcript_data(gene, genome_build)
+            
+                # Extract the exon information from the retrieved transcript data
+                exon_data = extract_exon_info(gene_transcript_data)
+                
+                # Write the extracted exon information to the BED file
+                for exon in exon_data:
+                    # Subtract 1 to zero-index the start position
+                    exon["exon_start"] -= 1
+                    # Each line in the BED file corresponds to an exon and its relevant details
+                    bed_file.write(f"{exon['chromosome']}\t{exon['exon_start']}\t{exon['exon_end']}\t"
+                                f"{exon['exon_number']}\t{exon['reference']}\t{exon['gene_symbol']}\n")
+
+                # log addition of exon data for each gene    
+                logging.info(f"Added exon data for {gene} to the BED file.")
+            
+            except Exception as e:
+                logging.error(f"Error processing {gene}: {e}")
+
+        # log message indicating that BED file has been successfully saved
+        logging.info(f"Data saved to {output_file}")
+    
+
+def bedtools_merge(panel_name, panel_version, genome_build):
+    """
+    Sorts and merges overlapping regions in a BED file generated by generate_bed_file.
+
+    Args:
+        panel_name (str): Name of the genomic panel.
+        panel_version (str): Version of the genomic panel.
+        genome_build (str): Genome build identifier (e.g., "GRCh38").
+
+    Returns:
+        None: Creates a merged BED file (e.g., `R59_v2_merged.bed`) in the same directory.
+
+    Dependencies:
+        Requires bedtools (available in PanelPal conda environment).
+
+    """
+    
+    # Define the input and output file names based on parameters
+    bed_file = f"{panel_name}_v{panel_version}_{genome_build}.bed"
+    merged_bed_file = f"{panel_name}_v{panel_version}_{genome_build}_merged.bed"
+
+    # Try running bedtools merge
+    try:
+        merge_command = f"bedtools sort -i {bed_file} | bedtools merge > {merged_bed_file}"
+        subprocess.run(merge_command, shell=True, check=True)
+        logging.info(f"Successfully sorted and merged BED file to {merged_bed_file}")
+    
+    # If an error is encountered log the error
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error during bedtools operation: {e}")
+
+
+
+####### TEST VARIABLES #######
+def main():
+    # Set panel id and version
+    panel_name = "R207"
+    panel_version = "4"
+    gene_list = [
+        "BRCA1","BRCA2","BRIP1","MLH1","MSH2","MSH6","PALB2","RAD51C","RAD51D","PMS2",
+        "AR","ATM","BARD1","CDH1","CHEK2","EPCAM","ESR1","MUTYH","NBN","PPM1D","PTEN",
+        "RAD54L","RRAS2","STK11","TP53","XRCC2"
+        ]
+    genome_build = "GRCh38"
+
+    # Generate bed files
+    generate_bed_file(gene_list, panel_name, panel_version, genome_build)
+    bedtools_merge(panel_name, panel_version, genome_build)
+
+if __name__ == "__main__":
+    main()
--- PanelPal/check_panel.py
+++ PanelPal/check_panel.py
@@ -5,143 +5,219 @@ import re
 import requests
 import logging
 import sys
-import os
-
-# Add parent directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+import time
+import json
 from accessories.panel_app_api_functions import get_response, get_name_version
 
-# Configure logging
-logging.basicConfig(
-	level=logging.DEBUG,  # logging level
-	format="%(asctime)s - %(levelname)s - %(message)s",
-	handlers=[
-		logging.FileHandler("logging/check_panel.log"),  # store logging output here
-	],
-)
 
-# Add a console handler for warnings and errors only
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.WARNING)  # Set console handler to warning level
-console_handler.setFormatter(
-	logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-)
-logging.getLogger().addHandler(console_handler)
+def setup_logging(log_file="logging/check_panel.log"):
+	"""
+	Set up logging configuration.
 
-# Get the logger instance
-logger = logging.getLogger()
+	This function configures the logging system to write logs to a file and also
+	outputs warnings and above to the console. The log file is specified by the
+	`log_file` argument (default is "logging/check_panel.log").
+
+	Args:
+	    log_file (str): Path to the log file. Defaults to "logging/check_panel.log".
+
+	Returns:
+	    logging.Logger: The configured logger object.
+
+	"""
+
+	logger = logging.getLogger()
+	logger.setLevel(logging.DEBUG)
+
+	file_handler = logging.FileHandler(log_file)
+	file_handler.setLevel(logging.DEBUG)
+	file_handler.setFormatter(
+		logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+	)
+
+	console_handler = logging.StreamHandler()
+	console_handler.setLevel(logging.WARNING)
+	console_handler.setFormatter(
+		logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+	)
+
+	logger.addHandler(file_handler)
+	logger.addHandler(console_handler)
+	return logger
 
 
 def parse_arguments():
 	"""Parse command-line arguments."""
-	argument_parser = argparse.ArgumentParser(description="Format a given panel ID.")
+	argument_parser = argparse.ArgumentParser(
+		description="Format a given panel ID."
+	)
 	argument_parser.add_argument(
 		"--panel_id", help="Panel ID e.g. R59, r59, or 59", required=True
 	)
 	return argument_parser.parse_args()
 
 
-def format_panel_id(input_id):
+def is_valid_panel_id(input_id):
 	"""
-	Formats the input panel ID by stripping whitespace, converting to uppercase,
-	and ensuring it starts with 'R' followed by digits.
+	Check if the input panel ID is valid.
+
+	   Valid panel IDs must match the pattern 'R' followed by one or more digits (e.g., "R59").
+
+	   Args:
+	       input_id (str): The panel ID to check.
+
+	   Returns:
+	       bool: True if the input ID is valid, False otherwise.
+
+	   Example:
+	       is_valid_panel_id("R59")  # Returns True
+	       is_valid_panel_id("59")   # Returns True
+	       is_valid_panel_id("r59")  # Returns True
+	       is_valid_panel_id("XYZ")  # Returns False
+
+	"""
+	return re.fullmatch(r"R\d+$", input_id) is not None
+
+
+def format_panel_id(input_id: str) -> str:
+	"""
+	Format the input panel ID to ensure it follows the correct format.
+
+	The function strips any surrounding whitespace, converts the ID to uppercase,
+	and ensures that the ID starts with 'R' followed by one or more digits.
+	If the input doesn't meet this format, it raises a `ValueError`.
 
 	Args:
-		input_id (str): The input panel ID to format.
+	    input_id (str): The panel ID to format (e.g., "59", "r59", or "R59").
 
 	Returns:
-		str: Formatted panel ID starting with 'R' and followed by digits.
+	    str: The correctly formatted panel ID (e.g., "R59").
 
 	Raises:
-		ValueError: If the input ID is not in the expected format.
-	"""
-	panel_id = input_id.strip().upper()
+	    ValueError: If the input panel ID is not in a valid format.
 
+	Example:
+	    formatted_id = format_panel_id("r59")
+	    print(formatted_id)  # Output: "R59"
+
+	"""
+
+	panel_id = input_id.strip().upper()
 	if not panel_id.startswith("R"):
 		panel_id = "R" + panel_id
-
-	if not re.fullmatch(r"R\d+", panel_id):
-		raise ValueError("Panel ID must be 'R' followed by digits (e.g., 'R59').")
-
+	if not is_valid_panel_id(panel_id):
+		raise ValueError(
+			"Panel ID must be 'R' followed by digits (e.g., 'R59')."
+		)
 	return panel_id
 
 
-def fetch_panel_info(formatted_id):
+def fetch_panel_info(
+	formatted_id: str, retries: int = 3, delay: int = 10
+) -> dict:
 	"""
-	Fetch the panel information from the API.
+	Fetch the panel information from the API with retry logic.
+
+	This function attempts to retrieve the panel name and version from the API. If the request
+	fails due to network issues, it will retry up to a specified number of times.
+
+	Args:
+	    formatted_id (str): The formatted panel ID (e.g., "R59").
+	    retries (int, optional): The number of retry attempts in case of failure. Defaults to 3.
+	    delay (int, optional): The delay in seconds between retries. Defaults to 10.
+
+	Returns:
+	    dict: A dictionary containing the panel's name and version:
+	        - 'name': The clinical indication or description of the panel.
+	        - 'version': The version of the panel.
+
+	Raises:
+	    requests.exceptions.RequestException: If the API request fails after the specified retries.
+	    KeyError: If the response does not contain the expected keys ('name' or 'version').
+
+	Example:
+	    panel_info = fetch_panel_info("R59")
+	    print(panel_info["name"], panel_info["version"])
+	"""
+
+	"""
+	Fetch the panel information from the API with retry logic.
 
 	Args:
 		formatted_id (str): The formatted panel ID.
+		retries (int): Number of retry attempts for network issues.
+		delay (int): Delay between retries in seconds.
 
 	Returns:
 		dict: Dictionary containing panel name and version.
 
 	Raises:
-		requests.exceptions.RequestException: If there's an error making the API request.
+		requests.exceptions.RequestException: If there's an error making the API request after retries.
 		KeyError: If the response does not contain the expected keys.
 	"""
-	try:
-		response = get_response(formatted_id)
-		panel_info = get_name_version(response)
-
-		# Ensure expected keys are in the response
-		if "name" not in panel_info or "version" not in panel_info:
-			raise KeyError(f"Response missing expected fields: 'name' or 'version'.")
-
-		return panel_info
-
-	except requests.exceptions.RequestException as e:
-		logging.error(f"Error contacting the API: {e}")
-		raise  # Re-raise exception for further handling in main()
-
-	except KeyError as e:
-		logging.error(f"API response error: {e}")
-		raise  # Re-raise exception for further handling in main()
+	for attempt in range(1, retries + 1):
+		try:
+			response = get_response(formatted_id)
+			panel_info = get_name_version(response)
+			if "name" not in panel_info or "version" not in panel_info:
+				raise KeyError("Missing 'name' or 'version' in response.")
+			return panel_info
+		except requests.exceptions.RequestException as e:
+			if attempt < retries:
+				logging.warning(
+					"Attempt %d/%d: Unable to reach API, reattempting connection in %d seconds...",
+					attempt,
+					retries,
+					delay,
+				)
+				time.sleep(delay)
+			else:
+				# Log a specific message for the final attempt failure
+				logging.error(
+					"Final attempt (%d/%d) failed: Unable to reach the API. Please check your connection.",
+					attempt,
+					retries,
+				)
 
 
 def main():
+	"""
+	Main entry point of the script.
+
+	   This function sets up the logger, parses the command-line arguments for the panel ID,
+	   formats the panel ID, retrieves panel information from the API, and prints the result.
+	   It also handles exceptions and logs errors accordingly.
+
+	"""
+
+	# Set up logger
+	logger = setup_logging()
+
 	# Gather command-line arguments
 	args = parse_arguments()
+	logger.debug("Parsed command-line arguments: panel_id=%s", args.panel_id)
 
 	try:
-		# Format the panel ID
 		formatted_id = format_panel_id(args.panel_id)
-		print(f"Panel ID: {formatted_id}")
-
-		# Fetch panel information from the API
 		panel_info = fetch_panel_info(formatted_id)
-		indication = panel_info["name"]
-		version = panel_info["version"]
 
-		# Output the result
-		print(f"Clinical indication: {indication}")
-		print(f"Latest version: {version}")
+		print(f"Panel ID: {formatted_id}")
+		print(f"Clinical Indication: {panel_info['name']}")
+		print(f"Latest Version: {panel_info['version']}")
 
 	except ValueError as e:
-		# Handle input format errors
-		print(f"Error: {e}", file=sys.stderr)
+		# Log the ValueError
+		logger.error("ValueError: %s", e)
 		sys.exit(1)
 
-	except requests.exceptions.RequestException as e:
-		# Handle network issues with API
-		print(
-			f"Network error: Unable to reach the API. Please check your connection.",
-			file=sys.stderr,
-		)
-		sys.exit(2)
-
 	except KeyError as e:
-		# Handle missing keys in the API response
-		print(
-			f"Error: Unexpected API response. Missing expected fields.", file=sys.stderr
-		)
+		# Log the missing field error
+		logger.error("Unexpected API response: Missing expected fields.")
 		sys.exit(3)
 
 	except Exception as e:
-		# Catch all other errors
-		logging.error(f"Unexpected error: {e}")
-		print(f"Unexpected error: {e}", file=sys.stderr)
+		# Log any other errors
+		logger.error(e)
 		sys.exit(99)
 
 
