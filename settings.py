3a0a54d8cf993be00d6963749f27627d330eb82b
PatrickWeller <patrick.weller@wales.nhs.uk>
2024-11-19 13:02:23 +0000

    Changed logger so stream handler is optional

--- settings.py
+++ settings.py
@@ -1,10 +1,23 @@
 import logging
 
 # Set up the basic configuration for the logger
+
+
+# Set this flag to True to enable console logging, False to disable it
+ENABLE_CONSOLE_LOGGING = True
+
+# Always log to a file
+handlers = [logging.FileHandler('app.log')]
+
+# Optionally log to the console if the above is set to True
+if ENABLE_CONSOLE_LOGGING:
+    handlers.append(logging.StreamHandler())
+
+# Set up the logger configuration
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[logging.FileHandler('app.log')]  # Only log to the file
+    handlers=handlers
 )
 
 # Create a logger instance
