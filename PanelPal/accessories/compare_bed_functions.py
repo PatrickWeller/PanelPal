02a9a9f4f3a40bbaac136139d3927e2f270ac65f
madysonic <69169272+madysonic@users.noreply.github.com>
2024-12-06 09:35:42 +0000

    Removed dummy data from script.

--- PanelPal/accessories/compare_bed_functions.py
+++ PanelPal/accessories/compare_bed_functions.py
@@ -2,6 +2,9 @@ import os
 from variant_validator_api_functions import generate_bed_file
 
 def bed_file_exists(panel_name, panel_version, genome_build):
+    """
+    Check if a bed file with a certain name already exists
+    """
     # Define the expected BED file name
     output_file = f"{panel_name}_v{panel_version}_{genome_build}.bed"
     # Check if the file exists
@@ -22,6 +25,7 @@ def read_bed_file(filename):
             # Concatenate all columns with underscores
             concatenated_entry = '_'.join(fields)
             bed_entries.add(concatenated_entry)
+
     return sorted(bed_entries)
 
 
@@ -39,7 +43,7 @@ def compare_bed_files(file1, file2):
 
     # Specify output folder 
     output_folder = "bedfile_comparisons"
-    
+
     # Create folder if does not exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -49,8 +53,8 @@ def compare_bed_files(file1, file2):
 
 
     # Find the differences
-    diff_file1 = sorted(set(bed_file1) - set(bed_file2))  # Entries in file1 but not in file2
-    diff_file2 = sorted(set(bed_file2) - set(bed_file1))  # Entries in file2 but not in file1
+    diff_file1 = sorted(set(bed_file1) - set(bed_file2))
+    diff_file2 = sorted(set(bed_file2) - set(bed_file1)) 
 
     # Column widths for formatting output
     col_widths = {
@@ -73,43 +77,3 @@ def compare_bed_files(file1, file2):
 
     print(f"Comparison complete. Differences saved in {output_file}")
 
-
-def main():
-    # dummy data
-    panel_name1 = "R207"
-    panel_version1 = "4"
-    gene_list1 = [
-        "BRCA1","BRCA2", "PTEN",
-        "RAD54L","RRAS2","STK11","TP53","XRCC2"
-        ]
-    genome_build = "GRCh38"
-
-    panel_name2 = "R207"
-    panel_version2 = "4.2"
-    gene_list2 = [
-        "BRCA1","BRCA2","MUTYH","NBN","PPM1D","PTEN",
-        "RAD54L","TP53","XRCC2, EGFR"
-        ]
-    genome_build = "GRCh38"
-    print("Generating beds")
-    generate_bed_file(gene_list1, panel_name1, panel_version1, genome_build)
-    generate_bed_file(gene_list2, panel_name2, panel_version2, genome_build)
-
-    print("Questioning existence")
-    bexist1 = bed_file_exists(panel_name1, panel_version1, genome_build)
-    bexist2 = bed_file_exists(panel_name2, panel_version2, genome_build)
-    bexist_false = bed_file_exists(panel_name2, 5, genome_build)
-
-    print(bexist1, bexist2, bexist_false)
-
-    file1 = "R207_v4_GRCh38.bed"
-    file2 = "R207_v4.2_GRCh38.bed"
-    print("Comparing: " + file1 + file2)
-
-    compare_bed_files(file1, file2)
-
-
-
-if __name__ == "__main__":
-    main()
-    
