29308825be8a4f0e287aae843db8bfd696156bda
ASendellPrice <ashley.sendell.price@gmail.com>
2024-12-06 15:48:27 +0000

    Addressing reviewer comments.

--- PanelPal/main.py
+++ PanelPal/main.py
@@ -1,3 +1,43 @@
+"""
+Main entry point for the PanelPal toolkit, which provides functionalities
+for querying and generating BED files for genomic panels.
+
+The script offers several primary subcommands:
+
+1. **check-panel**:
+   This subcommand allows users to check panel information based on the panel ID.
+   It queries panel details, such as name and version, from the PanelApp API.
+
+2. **generate-bed**:
+   This subcommand generates a BED file for a specified genomic panel.
+   It requires the panel ID, version, and genome build to generate the appropriate BED file.
+
+3. **compare-panel-versions**:
+   This subcommand allows users to compare two versions of the same panel ID.
+   It requires the panel ID, two version numbers, and can take a filter for gene status.
+
+Parameters
+----------
+None (this is an entry point for the script, which processes commands and arguments via argparse).
+
+Subcommands
+-----------
+- **check-panel**: Check panel information for a given panel ID.
+- **generate-bed**: Generate a BED file for a genomic panel.
+- **compare-panel-versions**: Compare two versions of a genomic panel.
+
+Examples
+--------
+To check panel information for a specific panel ID:
+    $ PanelPal check-panel --panel_id R59
+
+To generate a BED file for a specific panel, version, and genome build:
+    $ PanelPal generate-bed --panel_id R59 --panel_version 4 --genome_build GRCh38
+
+To query the gene differences between two versions of a panel:
+    $ PanelPal compare-panel-versions --panel_id R21 --versions 1.0 2.2 --status_filter green
+"""
+
 import argparse
 from check_panel import main as check_panel_main
 from generate_bed import main as generate_bed_main
@@ -30,8 +70,8 @@ Available Commands:
 def main():
     """Main function which gathers arguments and passes them to the relevant PanelPal command."""
     parser = argparse.ArgumentParser(
-        description="PanelPal: A toolkit for querying and generating genomic panel BED files.",
-        epilog="For more details, visit https://yourprojecturl.com"
+        description="PanelPal: A toolkit for helping UK labs implement the National Test Directory for rare disease",
+        epilog="For more details, visit https://github.com/PatrickWeller/PanelPal",
     )
 
     subparsers = parser.add_subparsers(
@@ -81,23 +121,26 @@ def main():
         help="Compare two versions of a genomic panel.",
     )
     parser_versions.add_argument(
-        "--panel", "-p",
+        "--panel",
+        "-p",
         type=validate_panel,
         required=True,
-        help='R number. Include the R.',
+        help="R number. Include the R.",
     )
     parser_versions.add_argument(
-        "--versions", "-v",
+        "--versions",
+        "-v",
         type=float,
         nargs=2,
         required=True,
-        help='Two panel versions. E.g. 1.1 or 69.23',
+        help="Two panel versions. E.g. 1.1 or 69.23",
     )
     parser_versions.add_argument(
-        '--status_filter', "-f",
+        "--status_filter",
+        "-f",
         choices=["green", "amber", "all"],
-        default='green',
-        help='Filter by gene status. Green only; green and amber; or all',
+        default="green",
+        help="Filter by gene status. Green only; green and amber; or all",
     )
 
     args = parser.parse_args()
