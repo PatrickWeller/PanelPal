6ecf757df4c0a19334a00b2413584ab132366a8d
PatrickWeller <patrick.weller@wales.nhs.uk>
2024-12-09 15:12:47 +0000

    Improved logging statements to incl gene filtering and gene numbers #79

--- PanelPal/compare_panel_versions.py
+++ PanelPal/compare_panel_versions.py
@@ -121,7 +121,9 @@ def main(panel=None, versions=None, status_filter='green'):
 
     # Get the list of genes for each panel version
     older_version_genes = get_genes(older_version_json, status_filter=status_filter)
+    logger.info("%d %s genes found on panel %s for version %s", len(older_version_genes), status_filter, panel, older_version)
     newer_version_genes = get_genes(newer_version_json, status_filter=status_filter)
+    logger.info("%d %s genes found on panel %s for version %s", len(newer_version_genes), status_filter, panel, newer_version)
 
     # Compare the 2 versions for removed or added genes
     removed = get_removed_genes(older_version_genes, newer_version_genes)
